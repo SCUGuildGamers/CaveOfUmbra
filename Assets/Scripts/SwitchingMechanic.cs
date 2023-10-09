42a6d8fbf7badf5ed364cbc3c3930f8c32185c67
hmuroavila <hmuroavila@scu.edu>
2023-10-09 10:39:52 -0700

    Switching Mechanic Complete
    Crude implementation of the switching mechanic is done.  Key features:
    Switching is based on object layers.  All relevant objects need to be in their respective layers in order to for them to be enabled/disabled.
    Switching completely disables/enables game objects.   Could be a problem in the future, since it also turns off all attached scripts and features.
    For efficiency, script collects all game objects at scene creation.  Therefore, if any objects are created after scene initialization, they will not be affected by the switching mechanic :/

---
+++ Assets/Scripts/SwitchingMechanic.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class SwitchingMechanic : MonoBehaviour
+{
+    // Initial variables, can be changed quickly in editor
+    public bool color_enabled = true;
+    public bool gray_enabled = false;
+    public int color_layer = 6;
+    public int gray_layer = 7;
+    private GameObject[] allObjects;
+
+    void Start()
+    {
+        // Saving all objects in scene onto an array for later use
+        allObjects = FindObjectsOfType<GameObject>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Switching initated");
+        // On SPACE key press
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            // Switch enabled value to whatever it was not
+            color_enabled = !color_enabled;
+            gray_enabled = !gray_enabled;
+            // Go through each GameObject previously gotten
+            foreach (GameObject obj in allObjects)
+            {
+                // Activate or Deactivate based on the new enabled value
+                if(obj.layer == color_layer)
+                {
+                    obj.SetActive(color_enabled);
+                }
+                else if(obj.layer == gray_layer)
+                {
+                    obj.SetActive(gray_enabled);
+                }
+            }
+        }
+    }
+}
